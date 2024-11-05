e6133ddc62215bce3265ac3e2325da4fc1812cf3
EdoTM <edokyactus@gmail.com>
2024-11-05 12:08:28 +0100

    feat: parallelism

--- gpt_lines.py
+++ gpt_lines.py
@@ -11,59 +11,23 @@ load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-input_folder = 'azure-json'
-output_folder = 'azure-gpt-lines'
-NUMBER_OF_CONTEXT_LINES = 5
 
 def main():
-    process_file("azure-json/4.json")
-    # for input_file in os.listdir(input_folder):
-    #     print(f'Processing {input_file}')
-    #     input_file_path = os.path.join(input_folder, input_file)
-    #
-    #     if '8' not in input_file:
-    #         continue
-    #
-    #     if os.path.isfile(input_file_path):
-    #         file_lines = [line.content for line in extract_lines_azure(input_file_path)]
-    #
-    #         corrected_lines = parallel_correct_text(file_lines, NUMBER_OF_CONTEXT_LINES)
-    #
-    #         output_path = os.path.join(output_folder, input_file.replace('.json', '.txt'))
-    #
-    #         with open(output_path, 'w', encoding='utf-8') as file:
-    #             for line in corrected_lines:
-    #                 file.write(line + '\n')
-    #
-    #         print(f'Corrected text saved in {output_path}')
+    input_folder = 'azure-json'
+    output_folder = 'azure-gpt-lines'
+    process_files_in_parallel(input_folder, output_folder)
 
-def process_file(filepath: str) -> None:
-    file_lines = "\n".join([line.content for line in extract_lines_azure(filepath)[:100]])
-    # print(file_lines)
-    # return
+
+def process_file_and_get_result(filepath: str) -> tuple[str, str]:
+    file_lines = "\n".join([line.content for line in extract_lines_azure(filepath)[:1]])
     response = call_api(file_lines)
-    print(response)
-
-
-if not os.path.exists(output_folder):
-    os.makedirs(output_folder)
+    return response, filepath
 
 
 @cache
 def get_correction_system_prompt() -> str:
     with open("gpt_prompt.md", "r") as f:
         return f.read()
-
-
-def get_formatted_input(context: str, line: str) -> str:
-    out = "<context>\n"
-    for l in context.split("\n"):
-        if l == line:
-            out += f"<line>{l}</line>\n"
-        else:
-            out += f"{l}\n"
-    out += "</context>"
-    return out
 
 
 def call_api(prompt):
@@ -77,38 +41,35 @@ def call_api(prompt):
             max_tokens=4096,
             temperature=0.2
         )
-        # print(response)
-        return response.choices[0].message.content.strip().replace("  \n", "\n")
+        content = response.choices[0].message.content
+        return "\n".join((l.strip() for l in content.split("\n")))
     except Exception as e:
         print(f"Error in API request: {e}")
         return None
 
 
-def process_text(i, line: str, context: str):
-    prompt = get_formatted_input(context, line)
-    response = call_api(prompt)
-    print(f"response: {response}")
-    return i, response
+def process_files_in_parallel(input_folder: str, output_folder: str):
+    filepaths = []
+    for filename in os.listdir(input_folder):
+        filepath = os.path.join(input_folder, filename)
+        if not os.path.isfile(filepath):
+            continue
+        filepaths.append(filepath)
 
-
-def parallel_correct_text(lines, x):
-    corrected_text = [None] * len(lines)
-
+    futures = []
     with ThreadPoolExecutor() as executor:
-        futures = {}
+        for filepath in filepaths:
+            future = executor.submit(process_file_and_get_result, filepath)
+            futures.append(future)
 
-        for i in range(len(lines)):
-            context_lines = lines[max(0, i - x) : min(len(lines), i + 1 + x)]
-            context = "\n".join(context_lines)
-            # context_text = " ".join(context_lines).replace('-\n', '').replace('\n', ' ')
-            line = lines[i]
-            futures[executor.submit(process_text, i, line, context)] = i
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
-        for future in tqdm(as_completed(futures), total=len(futures), desc="Processing lines"):
-            i, response = future.result()
-            corrected_text[i] = response
-
-    return corrected_text
+    for future in tqdm(as_completed(futures), total=len(futures), desc="Processing files"):
+        corrected_content, filepath = future.result()
+        output_filepath = filepath.replace(input_folder, output_folder).replace('.json', '.txt')
+        with open(output_filepath, 'w', encoding='utf-8') as file:
+            file.write(corrected_content)
 
 
 if __name__ == '__main__':
