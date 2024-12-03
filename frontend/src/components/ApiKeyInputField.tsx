e3113df6977397a616a8bd16a84a2298221629aa
efiocchi <edokyactus@gmail.com>
2024-12-03 15:48:39 +0100

    feat(frontend): add api key validation

---
+++ frontend/src/components/ApiKeyInputField.tsx
@@ -0,0 +1,68 @@
+import React, {useState, useEffect} from 'react';
+import {FaCheckCircle, FaTimesCircle} from 'react-icons/fa';
+
+export type ApiKeyValidationStatus = 'none' | 'validating' | 'success' | 'error';
+
+type ApiKeyInputFieldProps = {
+  initialValue: string;
+  validationStatus: ApiKeyValidationStatus;
+  onKeyUpdate: (key: string) => void;
+};
+
+export function ApiKeyInputField({
+                                   initialValue,
+                                   validationStatus,
+                                   onKeyUpdate,
+                                 }: ApiKeyInputFieldProps) {
+  const [text, setText] = useState(initialValue || "");
+  const [inputTimeoutId, setInputTimeoutId] = useState<number>();
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newKey = event.target.value;
+    setText(newKey);
+    if (inputTimeoutId) {
+      clearTimeout(inputTimeoutId);
+    }
+    const timeout = setTimeout(() => {
+      console.log("ciao");
+      onKeyUpdate(newKey);
+    }, 600);
+    setInputTimeoutId(timeout);
+  };
+
+  useEffect(() => {
+    // Clean up timeout on unmount
+    return () => {
+      if (inputTimeoutId) {
+        clearTimeout(inputTimeoutId);
+      }
+    };
+  }, [inputTimeoutId]);
+
+  return (
+    <div className="d-flex align-items-center">
+      <input
+        type="password"
+        className="form-control"
+        value={text}
+        onChange={handleInputChange}
+        placeholder="Enter your API key"
+      />
+      <div className="ms-2 d-flex align-items-center">
+        {validationStatus === 'validating' && <Spinner/>}
+        {validationStatus === 'success' && <FaCheckCircle className={"text-success"} size="1.5rem"/>}
+        {validationStatus === 'error' && <FaTimesCircle className={"text-danger"} size="1.5rem"/>}
+      </div>
+    </div>
+  );
+}
+
+function Spinner() {
+  return (
+    <div className="spinner-border text-primary spinner-border-sm" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </div>
+  );
+}
+
+export default ApiKeyInputField;
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,12 +1,41 @@
 import {Link} from 'react-router-dom';
 import {useState} from "react";
 import axiosInstance from "../axiosInstance.ts";
+import ApiKeyInputField, {ApiKeyValidationStatus} from "../components/ApiKeyInputField.tsx";
+
+const DUMMY_API_KEY = "api-key";
 
 function HomePage() {
-  const [apiKey, setApiKey] = useState('');
+  const [apiKey, setApiKey] = useState(localStorage.getItem('apiKey') && DUMMY_API_KEY || '');
+  const [keyValidationStatus, setKeyValidationStatus] = useState<ApiKeyValidationStatus>('none');
 
-  const updateApiKey = () => {
-    axiosInstance.defaults.headers.common['X-API-KEY'] = apiKey;
+  const isApiKeyValid = () => {
+    return (keyValidationStatus === 'success' || keyValidationStatus === 'none' && apiKey);
+  }
+
+  const updateApiKey = (key: string) => {
+    if (!key || key === DUMMY_API_KEY) {
+      return;
+    }
+    setApiKey(key);
+    localStorage.setItem('apiKey', key);
+    axiosInstance.defaults.headers.common['X-API-KEY'] = key;
+  }
+
+  const onApiKeyValidationTrigger = (key: string) => {
+    if (!key) {
+      setKeyValidationStatus('none');
+      return;
+    }
+    setKeyValidationStatus('validating');
+    axiosInstance.get('/validate-api-key', {headers: {'X-API-KEY': key}})
+      .then(() => {
+        setKeyValidationStatus('success');
+        updateApiKey(key);
+      })
+      .catch(() => {
+        setKeyValidationStatus('error');
+      });
   }
 
   return (
@@ -14,17 +43,25 @@ function HomePage() {
       <div className="m-auto">
         <h1 className="text-center mb-4">JUMPI</h1>
         <div className="d-flex gap-2">
-          <Link to="/search" className="btn btn-primary btn-lg mx-auto" onClick={updateApiKey}>
+          <Link
+            to={isApiKeyValid() ? "/search" : "#"}
+            className={"btn btn-primary btn-lg mx-auto" + (isApiKeyValid() ? "" : " disabled")}
+          >
             Search
           </Link>
-          <Link to="/upload" className="btn btn-primary btn-lg mx-auto" onClick={updateApiKey}>
+          <Link
+            to={isApiKeyValid() ? "/upload" : "#"}
+            className={"btn btn-primary btn-lg mx-auto" + (isApiKeyValid() ? "" : " disabled")}
+          >
             Upload
           </Link>
         </div>
         <div className="mt-4">
           <label htmlFor="apiKey" className="form-label">API Key:</label>
-          <input type="password" id="apiKey" className="form-control" placeholder="Enter your API key"
-                 onChange={(e) => setApiKey(e.target.value)} value={apiKey}
+          <ApiKeyInputField
+            validationStatus={keyValidationStatus}
+            onKeyUpdate={onApiKeyValidationTrigger}
+            initialValue={apiKey}
           />
         </div>
       </div>
