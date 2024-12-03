59a514ea1a79f0ba56cbe4a5d90346f1b2840dfe
efiocchi <edokyactus@gmail.com>
2024-12-03 10:38:37 +0100

    style: reformat file

--- backend/server.py
+++ backend/server.py
@@ -1,23 +1,23 @@
+import base64
+import concurrent.futures
+import json
 import logging
+import os
 import shutil
+from pathlib import Path
+from threading import Lock
+
 from dotenv import load_dotenv
 from flask import Flask, request, jsonify
-import concurrent.futures
-from pathlib import Path
-import json
-import os
-import base64
 from flask_cors import CORS
+from werkzeug.utils import secure_filename
 
+from aws.call_api import analyze_document as aws_analyze_document
+from azure.call_api import analyze_document as azure_analyze_document
 from database_utils.classes import Article, Magazine
 from database_utils.database import Database
 from database_utils.elastic import ElasticsearchDb
 from main import process_file
-from werkzeug.utils import secure_filename
-from aws.call_api import analyze_document as aws_analyze_document
-from azure.call_api import analyze_document as azure_analyze_document
-from threading import Lock
-
 
 app = Flask(__name__)
 
@@ -41,7 +41,6 @@ Path(REPORT_FOLDER).mkdir(parents=True, exist_ok=True)
 Path(GPT_FOLDER).mkdir(parents=True, exist_ok=True)
 
 
-
 def process_single_file(file):
     filename = secure_filename(file.filename)
     file_path = Path(IMAGE_FOLDER) / filename
@@ -49,8 +48,10 @@ def process_single_file(file):
     aws_analyze_document(file_path, AWS_FOLDER)
     azure_analyze_document(file_path, AZURE_FOLDER)
     json_file = Path(filename).with_suffix('.json').name
-    process_file(json_file, AZURE_FOLDER, AWS_FOLDER, GPT_FOLDER, REPORT_FOLDER, IMAGE_FOLDER, IMAGE_COMPARISON_FOLDER, file_path)
+    process_file(json_file, AZURE_FOLDER, AWS_FOLDER, GPT_FOLDER, REPORT_FOLDER, IMAGE_FOLDER, IMAGE_COMPARISON_FOLDER,
+                 file_path)
     return filename
+
 
 @app.route('/analyze-documents', methods=['POST'])
 def analyze_documents_endpoint():
@@ -59,7 +60,7 @@ def analyze_documents_endpoint():
 
     if 'files' not in request.files:
         return jsonify({"error": "No files provided"}), 400
-    
+
     files = request.files.getlist('files')
     if not files:
         return jsonify({"error": "No files selected"}), 400
@@ -73,14 +74,15 @@ def analyze_documents_endpoint():
     except json.JSONDecodeError:
         return jsonify({"error": "Invalid metadata format"}), 400
 
-    required_fields = ["name_magazine", "year", "publisher", "genre", "article_title", "article_author", "article_page_range"]
+    required_fields = ["name_magazine", "year", "publisher", "genre", "article_title", "article_author",
+                       "article_page_range"]
     for field in required_fields:
         if field not in metadata:
             return jsonify({"error": f"Missing required field: {field}"}), 400
 
     with file_processing_lock:
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            filenames = executor.map(process_single_file, files)        
+            filenames = executor.map(process_single_file, files)
 
         combined_text = []
         response_text = []
@@ -92,7 +94,7 @@ def analyze_documents_endpoint():
                 content = f.read()
             offsets.append(len(content) + (offsets[-1] if offsets else 0))
             combined_text.append(content)
-            content = f"{text_file.name:-^50}\n" + content + f"\n{'-'*50}\n"
+            content = f"{text_file.name:-^50}\n" + content + f"\n{'-' * 50}\n"
             response_text.append(content)
 
         image_base64_list = []
@@ -106,9 +108,9 @@ def analyze_documents_endpoint():
         if temp_folder.exists() and temp_folder.is_dir():
             shutil.rmtree(temp_folder)
             temp_folder.mkdir()
-    
+
     print("\n".join(combined_text))
-    
+
     extracted_text = "\n".join(combined_text)
 
     magazine = Magazine(
@@ -139,10 +141,12 @@ def analyze_documents_endpoint():
 
     return jsonify(response)
 
+
 @app.route('/query', methods=['GET'])
 def query_documents():
     query_params = request.args or {}
 
+    # noinspection PyTypeChecker
     magazine = Magazine(
         name=query_params.get('name_magazine'),
         year=query_params.get('year'),
@@ -152,6 +156,7 @@ def query_documents():
         articles=None
     )
 
+    # noinspection PyTypeChecker
     article = Article(
         title=query_params.get('article_title'),
         author=query_params.get('article_author'),
@@ -162,7 +167,6 @@ def query_documents():
     )
 
     res = Database.get_instance().query(magazine, article)
-
     return jsonify(res)
 
 
@@ -175,6 +179,7 @@ def setup_db():
     logging.info(info)
     if not db.is_connected():
         raise Exception("Could not connect to the database")
+
 
 if __name__ == '__main__':
     load_dotenv()
