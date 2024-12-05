7778f11b4b880003163d818775fde1ae1dce2a2b
efiocchi <edokyactus@gmail.com>
2024-12-05 13:00:00 +0100

    refactor(backend): extract and rename functions

--- backend/app/services/file_processor.py
+++ backend/app/services/file_processor.py
@@ -15,39 +15,60 @@ file_processing_lock = Lock()
 
 def process_files(files):
     with file_processing_lock:
-        with ThreadPoolExecutor() as executor:
-            filenames = executor.map(_process_file, files)
-
-        combined_text = []
-        offsets = []
-        for text_file in Path(Config.GPT_FOLDER).iterdir():
-            if not text_file.is_file():
-                continue
-            with text_file.open('r') as f:
-                content = f.read()
-            offsets.append(len(content) + (offsets[-1] if offsets else 0))
-            combined_text.append(content)
-
-        image_base64_list = []
-        image_paths = [Path(Config.IMAGE_COMPARISON_FOLDER) / filename for filename in filenames]
-        for image_path in image_paths:
-            with Path(image_path).open('rb') as image_file:
-                image_data = image_file.read()
-                image_base64_list.append(b64encode(image_data).decode('utf-8'))
-
+        filenames = _process_files_and_get_filenames(files)
+        combined_text, offsets = _get_text_and_page_offsets()
+        image_base64_list = _get_base64_image_representations(filenames)
         Config.flush_temp_dirs()
 
-    combined_text = "\n".join(combined_text)
     return combined_text, offsets, image_base64_list
 
 
-def _process_file(file):
-    filename = secure_filename(file.filename)
-    file_path = Path(Config.IMAGE_FOLDER) / filename
-    file.save(file_path)
+def _get_base64_image_representations(filenames):
+    image_base64_list = []
+    image_paths = [Path(Config.IMAGE_COMPARISON_FOLDER) / filename for filename in filenames]
+    for image_path in image_paths:
+        with Path(image_path).open('rb') as image_file:
+            image_data = image_file.read()
+            image_base64_list.append(b64encode(image_data).decode('utf-8'))
+    return image_base64_list
+
+
+def _get_text_and_page_offsets() -> tuple[str, list[int]]:
+    pages_contents = []
+    page_offsets = []
+    for text_file in Path(Config.GPT_FOLDER).iterdir():
+        if not text_file.is_file():
+            continue
+        with text_file.open('r') as f:
+            content = f.read()
+        page_offsets.append(len(content) + (page_offsets[-1] if page_offsets else 0))
+        pages_contents.append(content)
+    pages_contents = "\n".join(pages_contents)
+    return pages_contents, page_offsets
+
+
+def _process_files_and_get_filenames(files):
+    with ThreadPoolExecutor() as executor:
+        return executor.map(_process_file_and_get_filename, files)
+
+
+def _process_file_and_get_filename(file):
+    file_path = _save_file_on_disk_and_get_path(file)
     AwsTextractReader(file_path).read_to_file(Config.AWS_FOLDER)
     AzureDiReader(file_path).read_to_file(Config.AZURE_FOLDER)
 
-    json_file = Path(filename).with_suffix('.json').name
-    process_file(json_file, str(file_path))
-    return filename
+    json_filename = file_path.with_suffix('.json').name
+    process_file(json_filename, str(file_path))
+    return file_path.name
+
+
+def _save_file_on_disk_and_get_path(file) -> Path:
+    filename = secure_filename(file.filename)
+    file_path = Path(Config.IMAGE_FOLDER) / filename
+    file.save(file_path)
+    return file_path
+
+
+def _read_lines(file_path: Path) -> list[str]:
+    with open(file_path, 'r') as file:
+        return file.readlines()
