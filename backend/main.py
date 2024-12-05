0ae6a5f2bc009f491a2f542f54560f0248a13cb9
efiocchi <edokyactus@gmail.com>
2024-12-05 13:51:07 +0100

    refactor(backend): extract functions in main

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,34 @@
 import os
 
-from app import create_app
-from app.config import Config
 from dotenv import load_dotenv
 
-if __name__ == "__main__":
+from app import create_app, config
+
+
+def main():
     load_dotenv()
+
+    debug = get_boolean_env_var('DEBUG')
+    setup_config()
     app = create_app()
-    debug = os.getenv('DEBUG', 'False').lower() == 'true'
+
+    config.Config.create_temp_dirs()
+
     host = os.getenv('HOST', '0.0.0.0')
     port = int(os.getenv('PORT', 5123))
-    Config.create_temp_dirs()
     app.run(debug=debug, host=host, port=port)
+
+
+def setup_config(debug=False):
+    config.APP_CONFIG = config.Config(
+        ELASTIC_URL=os.getenv('ELASTIC_URL'),
+        DEBUG=debug
+    )
+
+
+def get_boolean_env_var(env_var: str) -> bool:
+    return os.getenv(env_var, 'False').lower() == 'true'
+
+
+if __name__ == "__main__":
+    main()
